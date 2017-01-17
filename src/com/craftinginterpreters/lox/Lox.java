4f82cf1b37871523a2a6da80d00259b241f73a54
Nick Cervantes <nicholaslcervantes@gmail.com>
2017-01-16 23:36:28 -0800

    4.1.1 - Error handling

--- src/com/craftinginterpreters/lox/Lox.java
+++ src/com/craftinginterpreters/lox/Lox.java
@@ -7,41 +7,66 @@ import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.List;
-import java.util.Map;
 
 public class Lox {
-	public static void main(String[] args) throws IOException {
-		if (args.length > 1) {
-			System.out.println("Usage: jlox [script]");
-		} else if (args.length == 1) {
-			runFile(args[0]);
-		} else {
-			runPrompt();
-		}
-	}
+    static boolean hadError = false;
 
-	private static void runFile(String path) throws IOException {
-		byte[] bytes = Files.readAllBytes(Paths.get(path));
-		run(new String(bytes, Charset.defaultCharset()));
-	}
+    public static void main(final String[] args) throws IOException {
+        if (args.length > 1) {
+            System.out.println("Usage: jlox [script]");
+        } else if (args.length == 1) {
+            runFile(args[0]);
+        } else {
+            runPrompt();
+        }
+    }
 
-	private static void runPrompt() throws IOException {
-		InputStreamReader input = new InputStreamReader(System.in);
-		BufferedReader reader = new BufferedReader(input);
+    private static void runFile(final String path) throws IOException {
+        final byte[] bytes = Files.readAllBytes(Paths.get(path));
+        run(new String(bytes, Charset.defaultCharset()));
 
-		while (true) {
-			System.out.print("> ");
-			run(reader.readLine());
-		}
-	}
+        // Indicate an error in the exit code.
+        if (hadError) {
+            System.exit(65);
+        }
+    }
 
-	private static void run(String source) {
-		Scanner scanner = new Scanner(source);
-		List<Token> tokens = scanner.scanTokens();
+    private static void runPrompt() throws IOException {
+        final InputStreamReader input = new InputStreamReader(System.in);
+        final BufferedReader reader = new BufferedReader(input);
 
-		// For now, just print the tokens.
-		for (Token token : tokens) {
-			System.out.println(token);
-		}
-	}
+        while (true) {
+            System.out.print("> ");
+            run(reader.readLine());
+            hadError = false;
+        }
+    }
+
+    private static void run(final String source) {
+        final Scanner scanner = new Scanner(source);
+        final List<Token> tokens = scanner.scanTokens();
+
+        // For now, just print the tokens.
+        for (final Token token : tokens) {
+            System.out.println(token);
+        }
+    }
+
+    static void error(final int line, final String message) {
+        report(line, "", message);
+    }
+
+    static private void report(final int line, final String where, final String message) {
+        /*
+         * Implementing error messages like this could be fun:
+         * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+         * Error: Unexpected "," in argument list.
+         *
+         * 15 | function(first, second,);
+         *                            ^-- Here.
+         * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+         */
+        System.err.println("[line " + line + "] Error" + where + ": " + message);
+        hadError = true;
+    }
 }
