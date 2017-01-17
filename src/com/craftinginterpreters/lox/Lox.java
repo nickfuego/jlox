6252ab8379f5a0f1bee918fb08a29f92435df0d1
Nick Cervantes <nicholaslcervantes@gmail.com>
2017-01-17 00:01:22 -0800

    4.2 - Tokens and Lexemes

--- src/com/craftinginterpreters/lox/Lox.java
+++ src/com/craftinginterpreters/lox/Lox.java
@@ -8,7 +8,7 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.List;
 
-public class Lox {
+public final class Lox {
     static boolean hadError = false;
 
     public static void main(final String[] args) throws IOException {
